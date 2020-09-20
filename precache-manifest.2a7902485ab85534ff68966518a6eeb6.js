dac89b84afc0400e9e4b93c761666858e03d9ed5
Kiriratanak Vorn <sir.ratanak@gmail.com>
2020-09-20 16:57:43 +0700

    Updates

---
+++ precache-manifest.2a7902485ab85534ff68966518a6eeb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec34f6733f8d9d0b6656c11e5f80baf",
+    "url": "/robo-monsters/index.html"
+  },
+  {
+    "revision": "8b22e3d94556152c85d7",
+    "url": "/robo-monsters/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "97e860d9920380906222",
+    "url": "/robo-monsters/static/js/2.62cb15f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-monsters/static/js/2.62cb15f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b22e3d94556152c85d7",
+    "url": "/robo-monsters/static/js/main.3e190fcd.chunk.js"
+  },
+  {
+    "revision": "a1c99efed488188fe681",
+    "url": "/robo-monsters/static/js/runtime-main.f05e6c2d.js"
+  }
+]);
